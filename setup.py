678c083738bd7c7657fced5dcc12a915a116f231
William Hilska <whilska@gmail.com>
2022-05-30 09:57:17 -0400

    creating remote repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='gabble',
+	author='William H. Hilska',
+	author_email='whilska@gmail.com',
+	version='0.1.0',
+	packages=find_packages(),
+	install_requires=['hilskapy']
+)
